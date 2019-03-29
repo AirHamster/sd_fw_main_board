be272ae277dc44788a58fb00e4aa5d110baad921
Ilya Zarubin <air_hamster@bk.ru>
2019-03-29 09:17:29 +0300

    added minmea code

--- main.c
+++ main.c
@@ -25,6 +25,9 @@
 #include "MPU9250.h"
 #include "neo-m8.h"
 
+uint8_t neo_rx_buff1[256];
+uint8_t neo_rx_buff2[256];
+struct minmea_sentence_rmc rmc;
 /*
  * Maximum speed SPI configuration (18MHz, CPHA=0, CPOL=0, MSb first).
  */
@@ -34,6 +37,16 @@ static const SPIConfig spi2_cfg = {
   GPIOC,
   GPIOC_MCU_CS,
   SPI_CR1_BR_2 | SPI_CR1_BR_1 | SPI_CR1_BR_0 | SPI_CR1_CPOL | SPI_CR1_CPHA,
+//  0,
+  0
+};
+
+static const SPIConfig spi2_cfg_neo = {
+  false,
+  NULL,
+  GPIOC,
+  GPIOC_MCU_CS,
+  SPI_CR1_BR_2 | SPI_CR1_BR_1 | SPI_CR1_BR_0,
 //  0,
   0
 };
@@ -67,33 +80,49 @@ static THD_FUNCTION(Thread1, arg) {
  *	MPU9250 and UBLOX NEO-M8P
  */
 
-static THD_WORKING_AREA(spi_thread_1_wa, 512);
+static THD_WORKING_AREA(spi_thread_1_wa, 4096);
 static THD_FUNCTION(spi_thread_1, p) {
 
   (void)p;
   int16_t accel_data[3];
   int16_t gyro_data[3];
   int16_t mag_data[3];
-  uint8_t rxbuf[101];
+  char rxbuf[401];
    //txbuf[0] = 0xF5;
   //txbuf[1] = 0xFF;
   int i = 0;
    chRegSetThreadName("SPI thread 1");
   while (true) {
-        chThdSleepMilliseconds(100);
+        chThdSleepMilliseconds(300);
         palSetLine(LINE_GREEN_LED);    /* LED ON.                          */
 
-    mpu_read_accel_data(&accel_data[0]);
-    mpu_read_gyro_data(&gyro_data[0]);
+ //   mpu_read_accel_data(&accel_data[0]);
+ //   mpu_read_gyro_data(&gyro_data[0]);
     //mpu_read_mag_data(&mag_data[0]);
-    neo_read_bytes(&SPID2, 100, rxbuf);
+    neo_read_bytes(&SPID2, 400, rxbuf);
+    rxbuf[401] = '\0';
     palClearLine(LINE_GREEN_LED);    /* LED OFF.                          */
     chThdSleepMilliseconds(100);
-    chprintf((BaseSequentialStream*)&SD1, "\nACCEL X: %d ACCEL_Y: %d ACCEL_Z: %d \r\nGYRO_X: %d GYRO_Y: %d GYRO_Z: %d \r\nMAG_X: %d MAG_Y: %d MAG_Z: %d \r\n\n",
-    									accel_data[0], accel_data[1], accel_data[2], gyro_data[0], gyro_data[1], gyro_data[2], mag_data[0], mag_data[1], mag_data[2]);
-    chprintf((BaseSequentialStream*)&SD1, "\n SPI:  ");
-    for (i = 0; i<100; i++){
-    	chprintf((BaseSequentialStream*)&SD1, "%x ", rxbuf[i]);
+    if (minmea_parse_rmc(&rmc, rxbuf)){
+    	//chprintf((BaseSequentialStream*)&SD1, "RMC found\n\r");
+        	//chprintf((BaseSequentialStream*)&SD1, "RMC time:           %d:%d:%d:%d\n\r", rmc.time.hours, rmc.time.minutes, rmc.time.seconds, rmc.time.microseconds);
+        	//chprintf((BaseSequentialStream*)&SD1, "RMC valid:          %d\n\r", rmc.valid);
+        	//chprintf((BaseSequentialStream*)&SD1, "RMC raw latitude:   %d %d\n\r", rmc.latitude.value, rmc.latitude.scale);
+        	//chprintf((BaseSequentialStream*)&SD1, "RMC raw longtitude: %d %d\n\r", rmc.longitude.value, rmc.longitude.scale);
+        	//chprintf((BaseSequentialStream*)&SD1, "RMC raw speed:      %d %d\n\n\r", rmc.speed.value, rmc.speed.scale);
+        	/*chprintf((BaseSequentialStream*)&SD1, "RMC fp latitude:    %d\n\r", minmea_rescale(&rmc.latitude, 1000));
+        	chprintf((BaseSequentialStream*)&SD1, "RMC fp longtitude:  %d\n\r", minmea_rescale(&rmc.longitude, 1000));
+        	chprintf((BaseSequentialStream*)&SD1, "RMC fp speed:       %d\n\r", minmea_rescale(&rmc.speed, 1000));
+        	chprintf((BaseSequentialStream*)&SD1, "RMC float latitude: %d\n\r", minmea_tocoord(&rmc.latitude));
+        	chprintf((BaseSequentialStream*)&SD1, "RMC float longtit:  %d\n\r", minmea_tocoord(&rmc.longitude));
+        	chprintf((BaseSequentialStream*)&SD1, "RMC float speed:    %d\n\n\r", minmea_tocoord(&rmc.speed));*/
+
+        }
+   // chprintf((BaseSequentialStream*)&SD1, "\nACCEL X: %d ACCEL_Y: %d ACCEL_Z: %d \r\nGYRO_X: %d GYRO_Y: %d GYRO_Z: %d \r\nMAG_X: %d MAG_Y: %d MAG_Z: %d \r\n\n",
+    //									accel_data[0], accel_data[1], accel_data[2], gyro_data[0], gyro_data[1], gyro_data[2], mag_data[0], mag_data[1], mag_data[2]);
+    chprintf((BaseSequentialStream*)&SD1, "\n\rSPI:  \n\r");
+    for (i = 0; i < 400; i++){
+    	chprintf((BaseSequentialStream*)&SD1, "%c", rxbuf[i]);
     }
   }
 }
@@ -117,7 +146,7 @@ int main(void) {
    * Activates the serial driver 1 using the driver default configuration.
    */
   sdStart(&SD1, NULL);
-  spiStart(&SPID2, &spi2_cfg);       /* Setup transfer parameters.       */
+  spiStart(&SPID2, &spi2_cfg_neo);       /* Setup transfer parameters.       */
 
 
   //spiStart(&SPID2, &spi2_cfg);
@@ -141,7 +170,7 @@ int main(void) {
 
   chThdCreateStatic(spi_thread_1_wa, sizeof(spi_thread_1_wa),
                       NORMALPRIO + 1, spi_thread_1, NULL);
-  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO + 1, Thread1, NULL);
+ // chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO + 1, Thread1, NULL);
   /*
    * Normal main() thread activity, in this demo it does nothing except
    * sleeping in a loop and check the button state.
